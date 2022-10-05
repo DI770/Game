ab50268470c15bd22745545be43a4850c0a6f088
DI770 <peter.karlsson1987@gmail.com>
2022-10-05 15:12:26 +0000

    random number system works

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ let _User
 gameChoice.forEach(gameChoice => gameChoice.addEventListener('click',(e) => {
     _User = e.target.id
     userDisplay.innerHTML = _User
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * gameChoice.length)
+// I chose .floor to round it to a full number. 
+// I chose to make it * gameChoice so that I didn´t need to think about the number it will eventually need in the random number.
+    console.log(randomNumber);
+}
