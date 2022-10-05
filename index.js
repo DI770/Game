42381cbccf176e296c0b1eae05287acc6bd99177
DI770 <peter.karlsson1987@gmail.com>
2022-10-05 20:17:30 +0000

    trying to make the buttons respond

--- index.js
+++ index.js
@@ -4,16 +4,35 @@ const resultDisplay = document.getElementById('result')
 const gameChoice  = document.querySelectorAll('button')
 
 let _User
+let _Computer
 
 gameChoice.forEach(gameChoice => gameChoice.addEventListener('click',(e) => {
     _User = e.target.id
     userDisplay.innerHTML = _User
-    generateComputerChoice()
+    computerChoice()
 }))
 
-function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * gameChoice.length)
-// I chose .floor to round it to a full number. 
-// I chose to make it * gameChoice so that I didn´t need to think about the number it will eventually need in the random number.
-    console.log(randomNumber);
+function computerChoice() {
+    const numberGenerator = Math.floor(Math.random() * gameChoice.length)
+// I chose '.floor' to round it to a full number. 
+// The reason made it '* gameChoice.length' is so that I didn´t need to think about the number it will eventually need, in the random number.
+
+    if (numberGenerator === 1) {
+        computerChoice = 'rock'
+    }
+    if (numberGenerator === 2) {
+        computerChoice = 'paper'
+    }
+    if (numberGenerator === 3) {
+        computerChoice = 'scissor'
+    }
+    if (numberGenerator === 4) {
+        computerChoice = 'lizard'
+    }
+    if (numberGenerator === 5) {
+        computerChoice = 'spock'
+    }
+
+    computerDisplay.innerHTML = computerChoice
 }
+
