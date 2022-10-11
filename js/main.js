fc6c82da74ee22293c5820cd35ee1fc8fc6557cd
DI770 <peter.karlsson1987@gmail.com>
2022-10-11 11:21:11 +0000

    adding functions

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button")
+})
+console.log("button");
+function _PlayGround () {
+
+}
+
+function _PlayRound () {
+
+}
+
+function _PlayerChoice () {
+
+}
+
+function _ComputerChoice () {
+
+}
+
